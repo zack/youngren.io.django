d1df32a6451323fc7e1c3b93e6ed07d72a0e9073
Zack Youngren <zyoungren@constantcontact.com>
2014-11-07 06:09:05 +0000

    before snapshot restore

---
+++ apps/core/static/core/color.js
@@ -0,0 +1,8 @@
+(function() {
+  (function(_this) {
+    return (function() {
+      return document.getElementById("content").style.borderColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    });
+  })(this)();
+
+}).call(this);
